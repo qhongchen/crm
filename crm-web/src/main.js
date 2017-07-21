690167ead53d5c300af49531219c87748c1d9352
qhongChen <273664794@qq.com>
2017-07-21 23:48:58 +0800

    feat : 在main.js中使用组件
    - vue中引用组件后用 Vue.use(xxxx)的方式使用组件
    - vuex需要创建一个store目录来存放相关的文件
    - axios的使用方式是替换vue中的$http对象
    - element-ui的css样式需要单独引入

--- crm-web/src/main.js
+++ crm-web/src/main.js
@@ -3,13 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import store from './store'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
+
+Vue.use(ElementUI)
+Vue.use(Vuex)
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  store,
+  ...App
 })
