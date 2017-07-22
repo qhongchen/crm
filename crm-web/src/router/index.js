9351e48ca95009a129e4f19826c43a5abf942a16
qhongChen <273664794@qq.com>
2017-07-22 10:05:40 +0800

    refactor : 修改主界面
    - 修改App.vue
    - 修改Hello.vue
    - 将router的/路径修改为Main.vue，并将Hello.vue挂载为Main.vue的子路由

--- crm-web/src/router/index.js
+++ crm-web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Main from  '../pages/Main.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,15 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Main',
+      component: Main,
+      children:[
+        {
+        path : '',
+        // 懒加载
+        component : resolve => require(['../components/Hello.vue'],resolve),
+      },
+      ]
     }
   ]
 })
